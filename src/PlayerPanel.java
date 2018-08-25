99c3de4b3a9f97340e3c416174377aeb6e322336
Shodgearo <Shodkvest@gmail.com>
2018-08-25 20:10:04 +0600

    Minimal tools

--- src/PlayerPanel.java
+++ src/PlayerPanel.java
@@ -1,5 +1,32 @@
-/**
- * Created by Александр on 25.08.2018.
- */
-public class PlayerPanel {
+// class for panel of player
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayerPanel extends JPanel {
+    JButton buttons[];
+
+    public PlayerPanel() {
+        initButtons();
+        initFrame();
+    }
+
+    private void initButtons() {
+        buttons = new JButton[11];
+
+        // Create buttons
+        for (int j = 0; j < buttons.length; j++) {
+            buttons[j] = new JButton("--");
+        }
+
+        // Adding buttons
+        for (int j = 0; j < buttons.length; j++) {
+            add(buttons[j]);
+        }
+    }
+
+    private void initFrame() {
+        setLayout(new GridLayout(11, 0));
+        setPreferredSize(new Dimension(120, 550));
+    }
 }
