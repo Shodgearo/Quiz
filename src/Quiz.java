58c7c72fa77bf19a4a3fd34e5ccfc7c907f56e4c
Shodgearo <Shodkvest@gmail.com>
2018-08-25 11:45:23 +0600

    Working version of class

--- src/Quiz.java
+++ src/Quiz.java
@@ -8,12 +8,32 @@ public class Quiz extends JFrame {
         new Quiz();
     }
 
+    final int WHIDTH_WINDOW = 600;
+    final int HIEGHT_WINDOW = 200;
     JPanel mainPane;
     JButton quiz1, quiz2, quiz3; //Кнопки, при нажатии на которые нас перенесёт на соответствующий этап.
 
     Quiz() {
         initPanel();
+        initButton();
         toolsWindow();
+    }
+
+    private void initButton() {
+        final int WHIDTH = 150;
+        final int HIEGHT = 60;
+
+        quiz1 = new JButton("Викторина первая");
+        quiz2 = new JButton("Викторина вторая");
+        quiz3 = new JButton("Викторина третья");
+
+        quiz1.setBounds(50, (HIEGHT_WINDOW / 2) - HIEGHT / 2, WHIDTH, HIEGHT);
+        quiz2.setBounds(50 + WHIDTH + 10, (HIEGHT_WINDOW / 2) - HIEGHT / 2, WHIDTH, HIEGHT);
+        quiz3.setBounds(50 + WHIDTH + 10 + 10 + WHIDTH, (HIEGHT_WINDOW / 2) - HIEGHT / 2, WHIDTH, HIEGHT);
+
+        mainPane.add(quiz1);
+        mainPane.add(quiz2);
+        mainPane.add(quiz3);
     }
 
     private void initPanel() {
@@ -27,6 +47,7 @@ public class Quiz extends JFrame {
         };
 
         add(mainPane);
+        mainPane.setLayout(null);
     }
 
     private Image loadImage() {
@@ -36,7 +57,8 @@ public class Quiz extends JFrame {
     private void toolsWindow() {
         setTitle("Quiz");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(600, 230));
+        setPreferredSize(new Dimension(WHIDTH_WINDOW, HIEGHT_WINDOW + 25));
+        setResizable(false);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
