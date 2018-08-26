223adde4bf55de5447f50ab88745841e3980dac3
Shodgearo <Shodkvest@gmail.com>
2018-08-26 12:14:54 +0600

    Standard filing in

--- src/Panel_Anime.java
+++ src/Panel_Anime.java
@@ -1,5 +1,42 @@
-/**
- * Created by Александр on 26.08.2018.
- */
-public class Panel_Anime {
+// Panel for tab anime theme
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel_Anime extends JPanel {
+    private JButton[][] cards;
+
+    public Panel_Anime() {
+        initComponents();
+        initPanel();
+    }
+
+    private void initComponents() {
+        String s;
+
+        cards = new JButton[3][5];
+
+        //Init
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j < 3) s = String.valueOf((1 + j) * 100);
+                else if (j == 3) s = "500";
+                else s = "1000";
+
+                cards[i][j] = new JButton(s);
+            }
+        }
+
+        //Add
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                add(cards[i][j]);
+            }
+        }
+    }
+
+    private void initPanel() {
+        setLayout(new GridLayout(3, 1, 5, 5));
+        setPreferredSize(new Dimension(850, 160));
+    }
 }
--- src/PlayerPanel_3.java
+++ src/PlayerPanel_3.java
@@ -1,5 +1,32 @@
-/**
- * Created by Александр on 26.08.2018.
- */
-public class PlayerPanel_3 {
+// Class for third quiz for create players
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PlayerPanel_3 extends JPanel {
+    private JTextField scoreField, nameFiled;
+
+    public PlayerPanel_3() {
+        initComponent();
+        initFrame();
+    }
+
+    private void initComponent() {
+        nameFiled = new JTextField(10);
+        nameFiled.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        nameFiled.setHorizontalAlignment((int) JTextField.CENTER_ALIGNMENT);
+
+        scoreField = new JTextField("0", 10);
+        scoreField.setFont(new Font("Times New Roman", Font.BOLD, 22));
+        scoreField.setHorizontalAlignment((int) JTextField.CENTER_ALIGNMENT);
+        scoreField.setEditable(false);
+
+        add(nameFiled);
+        add(scoreField);
+    }
+
+    private void initFrame() {
+        setLayout(new FlowLayout());
+        setPreferredSize(new Dimension(200, 100));
+    }
 }
