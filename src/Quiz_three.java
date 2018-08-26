2aa714fe1367d0b3662ce081c9bca4d7b623ce78
Shodgearo <Shodkvest@gmail.com>
2018-08-26 12:15:39 +0600

    Copy code from analog class

--- src/Quiz_three.java
+++ src/Quiz_three.java
@@ -1,5 +1,87 @@
-/**
- * Created by Александр on 26.08.2018.
- */
-public class Quiz_three {
+// Description third quiz
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+public class Quiz_three extends JFrame {
+    final int WIDTH_WINDOW = 1000;
+    final int HEIGHT_WINDOW = 800;
+
+    private JPanel panel, tabOut, panel_players;
+    private Panel_Anime panel_Anime;
+    private Panel_Sport panel_Sport;
+    private Panel_IT panel_IT;
+    private LinkedList<PlayerPanel_3> list;
+    private JButton button;
+
+    public Quiz_three() {
+        list = new LinkedList<PlayerPanel_3>();
+        initPanels();
+        initButton();
+        initPanelResult();
+        initWindow();
+    }
+
+    private void initPanelResult() {
+        tabOut = new JPanel();
+        tabOut.setBounds(70, 20, 870, 550);
+        tabOut.setBackground(new Color(182, 201, 193));
+        tabOut.setLayout(new GridLayout(3, 1, 20, 20));
+
+        addingTab();
+
+        panel.add(tabOut);
+    }
+
+    private void addingTab() {
+        panel_Anime = new Panel_Anime();
+        panel_Sport = new Panel_Sport();
+        panel_IT = new Panel_IT();
+
+        tabOut.add(panel_Anime);
+        tabOut.add(panel_Sport);
+        tabOut.add(panel_IT);
+    }
+
+    private void initButton() {
+        button = new JButton("+");
+        button.setBounds(10, 635, 50, 50);
+        button.addActionListener(new AddNewPlayer());
+        panel.add(button);
+    }
+
+    private void initPanels() {
+        panel = new JPanel();
+        panel_players = new JPanel();
+        panel_players.setLayout(new FlowLayout());
+        panel_players.setBackground(Color.WHITE);
+        panel_players.setBounds(100, 600, 800, 130);
+
+        panel.setLayout(null);
+        add(panel);
+        panel.add(panel_players);
+    }
+
+    private void initWindow() {
+        setTitle("Quiz two");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setPreferredSize(new Dimension(WIDTH_WINDOW, HEIGHT_WINDOW));
+        setResizable(true);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private class AddNewPlayer implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            list.add(new PlayerPanel_3());
+
+            panel_players.add(list.getLast());
+            pack();
+        }
+    }
 }
